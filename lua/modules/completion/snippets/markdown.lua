8182aece6767ea6b313d711f96129c96ebf5924e
bekaboo <18127878294@qq.com>
2023-02-02 23:52:06 -0600

    671: Fix: markdown header snippets.

--- lua/modules/completion/snippets/markdown.lua
+++ lua/modules/completion/snippets/markdown.lua
@@ -60,12 +60,12 @@ M.markers = {
 
 M.titles = {
   snip = funcs.add_attr({ condition = funcs.not_in_mathzone }, {
-    s({ trig = 'h1' }, { t '# ', i(1), t ' #' }),
-    s({ trig = 'h2' }, { t '## ', i(1), t ' ##' }),
-    s({ trig = 'h3' }, { t '### ', i(1), t ' ###' }),
-    s({ trig = 'h4' }, { t '#### ', i(1), t ' ####' }),
-    s({ trig = 'h5' }, { t '##### ', i(1), t ' #####' }),
-    s({ trig = 'h6' }, { t '###### ', i(1), t ' ######' }),
+    s({ trig = 'h1' }, { t '# ', i(1) }),
+    s({ trig = 'h2' }, { t '## ', i(1) }),
+    s({ trig = 'h3' }, { t '### ', i(1) }),
+    s({ trig = 'h4' }, { t '#### ', i(1) }),
+    s({ trig = 'h5' }, { t '##### ', i(1) }),
+    s({ trig = 'h6' }, { t '###### ', i(1) }),
   }),
 }
 
