5f7e16371dbd2ebf0f7dbd27e2565c57564d4b1d
bekaboo <18127878294@qq.com>
2023-02-03 03:25:34 -0600

    676: Mod: decrease telescope's width and height.

--- lua/modules/tools/configs.lua
+++ lua/modules/tools/configs.lua
@@ -51,8 +51,18 @@ M['telescope.nvim'] = function()
       selection_caret = '→ ',
       borderchars = require('utils.static').borders.single,
       layout_config = {
-        horizontal = { prompt_position = 'top', preview_width = 0.5 },
-        vertical = { prompt_position = 'top', preview_width = 0.5 }
+        horizontal = {
+          prompt_position = 'top',
+          width = 0.8,
+          height = 0.8,
+          preview_width = 0.5,
+        },
+        vertical = {
+          prompt_position = 'top',
+          width = 0.8,
+          height = 0.8,
+          preview_width = 0.5,
+        },
       },
       sorting_strategy = 'ascending',
       file_ignore_patterns = { '.git/', '%.pdf', '%.o', '%.zip' },
